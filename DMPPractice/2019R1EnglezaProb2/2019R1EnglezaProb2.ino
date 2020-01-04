388ecd2c700d31c9b6935462e789284f0fc227cd
Vicondrus <victor.padurean@yahoo.com>
2020-01-04 17:03:11 +0200

    Add files via upload

---
+++ DMPPractice/2019R1EnglezaProb2/2019R1EnglezaProb2.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+
+int data;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  readData();
+  Serial.println(data);
+  delay(3000);
+}
+
+void readData(){
+  data = 0;
+  char x;
+
+  Wire.beginTransmission(0x65);
+  Wire.write(1);
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x65,8);
+
+  while(!Wire.available()){}
+  x = Wire.read();
+
+  data = data * 256 + x;
+
+  Wire.beginTransmission(0x65);
+  Wire.write(0);
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x65,8);
+
+  while(!Wire.available()){}
+  x = Wire.read();
+
+  data = data * 256 + x;
+  
+}
+
